c78b862f96e4a8bc822fa50c5520aa8d463c026e
Aggelos <87412823+AggelosAst@users.noreply.github.com>
2024-06-24 13:19:43 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-/*
-	This tracks the location of the current busses (you can edit it to whichever bus you want) and tells you how close the bus is
-	to a bus stop. Using haversine, the distance is accurately detected and with a default threshold of 300m, the results are
-	sorted to the closest one to teh bus.
-*/
-
 import {Oasth} from "./libs/Oasth";
-import {Stop} from "./types/stopsAndDetails";
-import {BusLocationData} from "./types/BusLocationData";
-import haversine from "haversine"
-import {ClosestStop} from "./types/closestStop";
-import {Line} from "./types/line";
 
 const oasth = new Oasth({
 	options: {
@@ -17,6 +6,6 @@ const oasth = new Oasth({
 	}
 })
 
-oasth.getLines().then(async a => {
+oasth.getLines().then(async _ => {
 	//Let your imagination unleash!
 })
